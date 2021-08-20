f44db2e5b18170d6a5051fe7c71f2d9c82f6f49a
karapazar <omer.savas@hotmail.com.tr>
2021-08-20 09:00:13 +0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,7 +7,7 @@ var options = {
   router: { mode: 'history' },
   detectSystemDarkTheme: true,
   darkThemeToggler: true,
-  sourcePath: 'https://mikrogovernment.github.io/angaryos-docs/', 
+  sourcePath: 'https://mikrogovernment.github.io/angaryos-docs', 
   
   highlight: ['typescript', 'bash', 'json', 'markdown'],
 
@@ -22,3 +22,5 @@ var options = {
 };
 
 new Docute(options);
+
+alert(1);
