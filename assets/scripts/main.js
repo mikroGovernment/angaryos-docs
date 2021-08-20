f7e5784ec8adb0330a0b8e95c30dd0993307372c
karapazar <omer.savas@hotmail.com.tr>
2021-08-20 08:17:02 +0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,7 +7,7 @@ var options = {
   router: { mode: 'history' },
   detectSystemDarkTheme: true,
   darkThemeToggler: true,
-  sourcePath: 'angaryos-docs',
+  sourcePath: '/',
   
   highlight: ['typescript', 'bash', 'json', 'markdown'],
 
